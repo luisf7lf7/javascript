4146b31a751450e16ff496d765ef83235aa536da
Luis Bustos <lufebumu@outlook.com>
2023-02-10 22:47:42 -0500

    Asignand e inicializando variables, y mostrar objetos por consola

---
+++ variables.js
@@ -0,0 +1,23 @@
+var nombre = "Luis"; // var palabra reservada de JS
+
+// declarar
+
+var edad;
+
+// Inicializar
+
+edad = 27;
+
+var elementos = ["Computadora", "Celular"];
+
+var personas = {
+    nombre: "Luis",
+    edad: 30
+};
+
+console.log(elementos);
+console.log(personas);
+
+console.log(elementos[0]);
+console.log(personas['nombre']);
+console.log(personas.edad);
